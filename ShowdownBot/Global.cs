8ebee729664831d014887eb99c69513d5577d9e9
Steven <therixy@gmail.com>
2015-07-25 13:50:17 -0400

    intit

---
+++ ShowdownBot/Global.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShowdownBot
+{
+    /// <summary>
+    /// Contains variables utilized by multiple classes.
+    /// </summary>
+  public static  class Global
+    {
+        // //Helper Information
+        //Colors
+        public static ConsoleColor warnColor = ConsoleColor.Yellow;
+        public static ConsoleColor okColor = ConsoleColor.Green;
+        public static ConsoleColor sysColor = ConsoleColor.Cyan;
+        public static ConsoleColor errColor = ConsoleColor.Red;
+        public static ConsoleColor botInfoColor = ConsoleColor.Magenta;
+        public static ConsoleColor defaultColor = ConsoleColor.White;
+
+      
+    }
+}
+       

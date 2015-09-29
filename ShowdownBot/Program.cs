7d31c9b6bfc6ca0ac3eaf396c87cf885cf237767
Steven <therixy@gmail.com>
2015-09-28 21:27:50 -0400

    redid configuration settings
    Reworked the config file to be a plaintext file, out of personal dislike
    of the app.config file. Configuration follows a [key]=value format.

--- ShowdownBot/Program.cs
+++ ShowdownBot/Program.cs
@@ -23,37 +23,9 @@ namespace ShowdownBot
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-           // Thread mainThread = new Thread(new ThreadStart(Application.Run(new Consol())));
-            //Consol cmd = new Consol();
             Application.Run(new Consol());
-           // new Thread(() => Application.Run(new Consol())).Start();
-            
-
-            //Thread thread = new Thread(() => Application.Run(cmd));
-            //thread.SetApartmentState(ApartmentState.STA);
-            //thread.Start();
-            //while (true)
-            //{
-            //    string line = Console.ReadLine();
-
-            //    // Action updateText = () => label.Text = line;
-            //    //cmd.Invoke(new Action(() => cmd.Parse(line)));
-
-            //}
             
         }
 
-        //private void startAndLoop()
-        //{
-        //    Consol cmd = new Consol();
-        //    new Thread(() => Application.Run(cmd)).Start();
-        //    while (true)
-        //    {
-        //        string line = Console.ReadLine();
-
-        //       // Action updateText = () => label.Text = line;
-        //        cmd.Invoke(new Action (() => cmd.Parse(line)) );
-        //    }
-        //}
     }
 }

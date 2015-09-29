548f460e58670aa0acf932bbd9828262958a3fdb
Steven <therixy@gmail.com>
2015-09-28 21:27:50 -0400

    redid configuration settings
    Reworked the config file to be a plaintext file, out of personal dislike
    of the app.config file. Configuration follows a [key]=value format.

---
+++ ShowdownBot/Pokemon.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShowdownBot
+{
+    public enum Types
+    {
+        FIRE,
+        WATER,
+        GRASS,
+        ICE,
+        DARK,
+        STEEL,
+        FAIRY,
+        POISON,
+        PYSCHIC,
+        BUG,
+        FLYING,
+        GROUND,
+        ELECTRIC,
+        DRAGON,
+        NORMAL,
+        GHOST,
+        ROCK,
+        FIGHTING
+    }
+    /// <summary>
+    /// Class to hold information on pokemon. Eventually this will be used to fill out a pokedex for use
+    /// with the bot's analytic functions. Alas for now...
+    /// </summary>
+    class Pokemon
+    {
+      
+    }
+}

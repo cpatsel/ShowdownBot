0d338dfc6c7ed7af5d0adb4b5bfa217772580a34
Steven <therixy@gmail.com>
2016-06-24 02:37:57 -0400

    completely reworked everything
    goodbye watin, hello selenium

---
+++ ShowdownBot/BotModule.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace ShowdownBot
+{
+    /// <summary>
+    /// Superclass for all botmodules
+    /// </summary>
+    class BotModule
+    {
+        protected State activeState;
+        protected IWebDriver browser;
+        protected Bot manager;
+        protected Consol c;
+        protected string format;
+        public BotModule(Bot m, IWebDriver b)
+        {
+
+            manager = m;
+            browser = b;
+
+            init();
+        }
+
+        public virtual void init()
+        {
+            activeState = State.IDLE;
+            c = manager.getConsole();
+
+        }
+        public virtual void Update()
+        {
+           
+            if (activeState == State.IDLE)
+            {
+                System.Threading.Thread.Sleep(5000);
+            }
+            else if (activeState == State.CHALLENGE)
+            {
+                challengePlayer(manager.getOwner(), format);
+            }
+            else if (activeState == State.BATTLE)
+            {
+                battle();
+            }  
+            
+        }
+
+        public virtual void battle()
+        {
+            //battle logic goes here.
+        }
+
+        /// <summary>
+        /// Sends a challenge to a player.
+        /// If no player is specified, it defaults to owner.
+        /// </summary>
+        /// <param name="b"></param>
+        private void challengePlayer(string user, string format)
+        {
+            string player = user;
+
+            c.write("Searching for " + player);
+            System.Threading.Thread.Sleep(2000);
+            browser.FindElement(By.Name("finduser")).Click();
+            System.Threading.Thread.Sleep(2000);
+            IWebElement e = browser.FindElement(By.Name("data"));
+            e.SendKeys(player);
+            System.Threading.Thread.Sleep(2000);
+            e.Submit();
+            System.Threading.Thread.Sleep(2000);
+            c.write("Contacting user for OU battle");
+            System.Threading.Thread.Sleep(2000);
+            browser.FindElement(By.Name("challenge")).Click();
+            System.Threading.Thread.Sleep(2000);
+            browser.FindElement(By.Name("format")).Click();
+            System.Threading.Thread.Sleep(2000);
+            browser.FindElement(By.CssSelector("button[name='selectFormat'][value='" + format + "']")).Click();
+            System.Threading.Thread.Sleep(2000);
+            browser.FindElement(By.Name("makeChallenge")).Click();
+            ////TODO: implement a way to select alternate teams/ have more than one team.
+            c.writef("Battle starting!", Global.botInfoColor);
+            changeState(State.BATTLE);
+
+        }
+
+
+        #region Battle Information Functions
+        /// <summary>
+        /// Checks whether it's possible to switch.
+        /// </summary>
+        /// <returns>can switch?</returns>
+         protected bool checkSwitch()
+        {
+            if (browser.FindElements(By.Name("chooseMove")).Count != 0 &&
+                browser.FindElements(By.Name("chooseSwitch")).Count !=0 &&
+                browser.FindElements(By.Name("undoChoice")).Count != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+         /// <summary>
+         /// Checks the bot's ability to select a move.
+         /// Bot prioritizes making moves over switching (for now)
+         /// </summary>
+         /// <param name="b"></param>
+         /// <returns>Can select a move?</returns>
+         protected bool checkMove()
+         {
+             
+              if (browser.FindElements(By.Name("chooseMove")).Count != 0)
+                     return true;
+                 else
+                     return false;
+             
+         }
+
+
+         protected Move[] getMoves()
+         {
+             //todo deal with moves with no pp/disabled
+             Move[] moves = new Move[4];
+             for (int i = 0; i < 4; i++)
+             {
+                 var b = browser.FindElement(By.CssSelector("button[name='chooseMove'][value='"+(i+1).ToString()+"']"));
+                 string[] html = b.GetAttribute("outerhtml").Split(new string[]{"data-move=\""},StringSplitOptions.None);
+                 var nametag = Array.Find(html, s => s.StartsWith("data-move"));
+                 string[] name = html[1].Split('"');
+                 string[] temp = b.GetAttribute("class").Split('-');
+                 string type = temp[1];
+
+                // moves [i] =
+                 
+                 Move m;
+                 if (Global.moves.ContainsKey(name[0]))
+                     m = Global.moves[name[0]];
+                 else
+                 {
+                     c.writef("Unknown move " + name[0], Global.warnColor);
+                     m = new Move(name[0], Global.types[type.ToLower()]);
+                 }
+                 moves[i] = m;
+                 //   moves[i] = lookupMove(name[0], Global.types[type.ToLower()]);
+                  c.writef("Move " + i.ToString() + " " + name[0], Global.botInfoColor);
+
+             }
+             return moves;
+         }
+
+
+        /// <summary>
+        /// Gets either active pokemon, but defaults to getting
+        /// the opponent's (rightbar)
+        /// </summary>
+        /// <param name="barclass">leftbar retrives the Player's active Pokemon</param>
+        /// <returns></returns>
+         protected Pokemon getActivePokemon(string barclass="rightbar")
+         {
+             //I feel like there's an easier way to do this.
+
+             c.write("Getting active Pokemon");
+             var elems = browser.FindElement(By.ClassName(barclass));
+             IList<IWebElement> ticon = elems.FindElements(By.ClassName("teamicons"));
+             string temp = parseNameFromPage(ticon);
+             if (temp == "0")
+             {
+                //error!
+                 return null;
+             }
+             ////Found the name, now look it up in the dex.
+             c.write("The current pokemon is "+temp);
+             Pokemon p = Global.lookup(temp);
+           
+             return p;
+         }
+         protected Pokemon updateYourPokemon()
+         {
+             Pokemon p = getActivePokemon("leftbar");
+             //do other stuff that may be useful here
+             return p;
+         }
+
+         protected string parseNameFromPage(IList<IWebElement> ticons)
+         {
+             for(int i = 0; i<ticons.Count;i++)
+             {
+                 IWebElement e = ticons[i];
+                 IList<IWebElement> elems = e.FindElements(By.ClassName("pokemonicon"));
+                 foreach (IWebElement s in elems)
+                 {
+                    if (s.GetAttribute("title").Contains("(active)"))
+                     {
+                         string[] name = s.GetAttribute("title").Split(' ');
+                         //Nicknamed pokemon appear in the html as "Nickname (Pokemon) (active)"
+                         //this means that the pokemon's name should be N-2, which should hold
+                         //true even for non-named mons.
+                         string n_name = name[name.Length - 2].Trim('(', ')'); //gets a sanitized name.
+                         return n_name.ToLower();
+                     }
+                 }
+             }
+             
+             return "0"; //return indicator that we did not find it.
+         }
+
+
+         /// <summary>
+         /// Randomly selects a pokemon.
+         /// </summary>
+         /// <returns>Index of pokemon.</returns>
+         protected int pickPokeRandomly()
+         {
+             Random rand = new Random();
+
+             HashSet<int> exclude = new HashSet<int>();
+             int choice = rand.Next(1, 5);
+             c.write("Choosing new pokemon");
+             choice = rand.Next(1, 5);
+            
+             while (browser.FindElements(By.CssSelector("button[value='"+choice.ToString()+"']")).Count == 0)
+             {
+                 c.writef("Bad pokemon " + choice.ToString() + ". Rolling for another.", Global.botInfoColor);
+
+                 exclude.Add(choice); //Steer it in the right direction by removing bad choices.
+                 choice = GetRandomExcluding(exclude, 1, 5);
+
+             }
+             return choice;
+         }
+
+         /// <summary>
+         /// Gets a random number from the range, excluding all numbers in the hash set.
+         /// </summary>
+         /// <param name="ex">set of excluded numbers</param>
+         protected int GetRandomExcluding(HashSet<int> ex, int min, int max)
+         {
+             var exclude = ex;
+             var range = Enumerable.Range(min, max).Where(i => !exclude.Contains(i));
+
+             var rand = new System.Random();
+             int index = rand.Next(min - 1, (max - 1) - exclude.Count);
+             return range.ElementAt(index);
+         }
+
+         protected bool checkBattleEnd()
+         {
+             if (browser.FindElements(By.Name("closeAndMainMenu")).Count != 0)
+             {
+                 //The match is over
+                 c.writef("The battle has ended! Returning to main menu.", Global.botInfoColor);
+
+                 activeState = State.IDLE;
+                 return true;
+             }
+             return false;
+
+         }
+
+         /// <summary>
+         /// Should be used when exiting a battle, prematurely or otherwise.
+         /// </summary>
+         /// <param name="b"></param>
+         /// <param name="forfeit">Go through steps to forfeit match?</param>
+         /// <returns>Whether it forfeited</returns>
+         protected bool goMainMenu(bool forfeit)
+         {
+
+             if (forfeit)
+             {
+                 //force the browser to click the exit button.
+                 browser.FindElement(By.ClassName("closebutton")).Click();
+                 return true;
+             }
+             else
+                 return false;
+         }
+
+        #endregion
+
+
+         
+
+         public void changeState(State ns)
+        {
+            activeState = ns;
+        }
+        public State getState()
+        {
+            return activeState;
+        }
+
+
+        
+
+        protected void wait(int timeInMiliseconds)
+        {
+            System.Threading.Thread.Sleep(timeInMiliseconds);
+        }
+        protected void wait()
+        {
+            //basic wait of 2 seconds
+            wait(2000);
+        }
+            
+    }
+}
---
+++ ShowdownBot/modules/AnalyticModule.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace ShowdownBot.modules
+{
+    class AnalyticModule : BotModule
+    {
+        public enum LastBattleAction
+        {
+            ACTION_ATTACK_SUCCESS,
+            ACTION_ATTACK_FAILURE,
+            ACTION_STATUS,
+            ACTION_BOOST,
+            ACTION_SWITCH
+        };
+        private LastBattleAction lastAction =LastBattleAction.ACTION_ATTACK_SUCCESS;
+
+        public AnalyticModule(Bot m, IWebDriver b) : base(m,b)
+        {
+            format = "ou";
+            
+        }
+
+        
+        public override void battle()
+        {
+            int turn = 1;
+            int lastTurn = 1;
+
+            //Select lead
+            string lead;
+            c.writef("Selecting first pokemon as lead", Global.botInfoColor);
+            System.Threading.Thread.Sleep(5000); //let page load
+
+            //TODO: actually pick this analytically.
+            lead = browser.FindElement(By.CssSelector("button[name='chooseTeamPreview'][value='0']")).Text;
+            browser.FindElement(By.CssSelector("button[name='chooseTeamPreview'][value='0']")).Click();
+
+            //while (browser.Eval("$('.whatdo').text()") == "How will you start the battle?")
+            //{
+
+            //        lead = SelectLead();
+            //}
+
+            Pokemon active = Global.lookup(lead);
+            Pokemon enemy = null;
+            do
+            {
+                    if (turn != lastTurn)
+                    {
+                        enemy = getActivePokemon();
+                        active = updateYourPokemon();
+                    }
+                    battleAnalytic(ref active, enemy, ref turn);
+                
+
+            } while (activeState == State.BATTLE); 
+        }
+
+        private bool battleAnalytic(ref Pokemon active, Pokemon enemy, ref int turn)
+        {
+
+            int moveSelection;
+            int pokeSelection;
+            if (enemy == null)
+            {
+                if (checkSwitch())
+                {
+
+                    browser.FindElement(By.CssSelector
+                        ("button[value='" + pickPokeRandomly().ToString() + "'][name='chooseSwitch']"));
+                    
+                    //todo change this to analytic pick
+                }
+                else
+                    return false;
+            }
+            /* 
+             * small ( maybe <10% ) chance of doing something random (so as to not be entirely predictable)
+             * first we do risk assessment
+             *      if we should switch, find most suitable candidate
+             *  then pick a move
+             *  firstly, preempt set-up if afflicable/relevant
+             *  
+             *      if sweeper
+             *          check for nullifying abilits like wonder guard/levitate etc.
+             *          avoid picking an attack nullified by opponent's type
+             *          pick the most powerful attack against the enemy's types
+             *      if support/cleric
+             *          check field for already present status, and avoid doubling up.
+             *      
+             *      else if all choices are equally bad/good just pick a random one.   
+             * 
+            */
+
+            if (checkBattleEnd())
+            {
+                return true;
+            }
+
+            //Switch if fainted
+            else if (checkSwitch())
+            {
+                active = pickPokeAnalytic(enemy);
+                if (active == null)
+                {
+                    c.writef("Can't find new pokemon, unable to continue.", "[ERROR]", Global.errColor);
+                    return true;
+                }
+                return false;
+            }
+            //Preemptively switch out of bad situations
+            else if (needSwitch(active, enemy))
+            {
+
+                c.writef("I'm switching out.", "Turn " + turn.ToString(), Global.botInfoColor);
+                Pokemon temp = pickPokeAnalytic(enemy);
+                if (temp == null)
+                {
+                    c.writef("Couldn't pick a pokemon. Going with moves instead.", "[!]", Global.warnColor);
+                    pickMoveAnalytic(active, enemy);
+
+                }
+                else
+                {
+                    active = temp;
+                }
+                turn++;
+                return false;
+
+            }
+            else if (checkMove())
+            {
+                if (browser.FindElements(By.Name("megaevo")).Count !=0)
+                    browser.FindElement(By.Name("megaevo")).Click();
+                //for now, automatically activate mega
+                c.writef("I'm picking move " + pickMoveAnalytic(active, enemy), "Turn " + turn.ToString(), Global.botInfoColor);
+                c.writef("Last Move: " + lastAction.ToString(), "DEBUG", Global.okColor);
+                turn++;
+            }
+
+            else
+                System.Threading.Thread.Sleep(2000);
+
+            return false;
+        }
+
+        private Pokemon pickPokeAnalytic(Pokemon enemy)
+        {
+            //Loop over all pokemon
+            int bestChoice = 1000;
+            float highestdamage = 5000f;
+            
+            for (int i = 1; i <= 5; i++)
+            {
+                Pokemon p = Global.lookup(browser.FindElement(By.CssSelector("button[name='chooseSwitch'][value='" + i.ToString() + "']")).Text);
+                if (bestChoice == 1000)
+                    bestChoice = i; //set a default value that can be accessed.
+                float temp = p.matchup(enemy);
+                //negate the return here in order to coincide with the defensive switching in the loop above.
+                //this should prevent eternally switching pokemon
+                 temp += enemy.checkKOChance(p);
+                 if (temp < highestdamage)
+                 {
+                     highestdamage = temp;
+                     bestChoice = i;
+                 }
+                 c.writef(p.name + " value:" + temp, "[DEBUG]", Global.okColor);
+
+
+
+
+            }
+            var b = browser.FindElement(By.CssSelector("button[name=chooseSwitch][value=" + bestChoice.ToString() + "']"));
+            Pokemon nextPoke = Global.lookup(b.Text);
+            b.Click();
+            return nextPoke;
+        }
+
+        private string pickMoveAnalytic(Pokemon you, Pokemon enemy)
+        {
+            float[] rankings = new float[4]; //ranking of each move
+            float bestMove = 0f;
+            int choice = 1;
+            float risk = you.matchup(enemy);
+            Move[] moves = getMoves();
+            for (int i = 0; i < 4; i++)
+            {
+                //For now, only determine the best attacking move.
+                if (moves[i].bp == 0)
+                {
+                    if (moves[i].boost && (lastAction == LastBattleAction.ACTION_BOOST))
+                        rankings[i] = 0;
+                    //placeholder
+                    else if (moves[i].boost && risk <= 1)
+                        rankings[i] = 2;
+
+
+                }
+                else if (moves[i].bp != 0)
+                {
+                    rankings[i] = you.damageCalcTotal(moves[i], enemy);
+
+
+                }
+                c.writef(moves[i].name + "'s rank: " + rankings[i].ToString(), "[DEBUG]", Global.okColor);
+
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (rankings[i] > bestMove)
+                {
+                    bestMove = rankings[i];
+                    choice = i + 1;
+                }
+            }
+
+            //figure out what move we've chosen
+            Move chosenMove = moves[choice - 1];
+            if (chosenMove.boost) lastAction = LastBattleAction.ACTION_BOOST;
+            else
+                lastAction = LastBattleAction.ACTION_ATTACK_SUCCESS;
+            if (checkMove())
+            {
+                //  mainBrowser.Eval("$('button[name=chooseMove][value=" + choice.ToString() + "]').click()");
+                return chosenMove.name;
+            }
+            else
+                return "no move";
+
+        }
+
+        private bool needSwitch(Pokemon you, Pokemon enemy)
+        {
+            if (isLastMon())
+                return false;
+            //if the pokemon is at low health, don't bother
+            //WatiN.Core.Element e = mainBrowser.Element(Find.ByClass("critical"));
+            //if (e.Exists)
+            float tolerance = 2.5f;
+            if (you.matchup(enemy) > tolerance)
+                return true;
+            else return false;
+        }
+        private bool isLastMon()
+        {
+            int totalMons = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+                //    if (browser.Button(Find.ByValue(i.ToString()) && Find.ByName("chooseSwitch")).Exists)
+                totalMons++;
+            }
+            if (totalMons == 0)
+                return true;
+            else
+                return false;
+        }
+
+    }
+}

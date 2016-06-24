7dd3e5b5b62e267c11f3a1a2cecec5d720e1fc5f
Deviach <therixy@gmail.com>
2016-06-24 16:28:40 -0400

    bugfix for random

--- ShowdownBot/Bot.cs
+++ ShowdownBot/Bot.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using WatiN.Core;
 using System.Drawing;
 using System.Net.NetworkInformation;
 using System.Windows.Forms;

e65aca5b12bc93fd9df36f0c18afb2d64f9c641e
Deviach <therixy@gmail.com>
2016-07-20 22:12:40 -0400

    split console into specialised files

---
+++ ShowdownBot/Console.CmdParser.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace ShowdownBot
+{
+    partial class Consol
+    {
+        public void Parse(string t)
+        {
+            string[] args;
+            args = Regex.Split(t, " ");
+            if (args[0] == "start")
+            {
+
+                writef("Starting bot...", "[SYSTEM]", Global.sysColor);
+                botUseCommand(() => bot.Start(true));
+            }
+            else if (args[0] == "startf")
+            {
+
+                writef("Starting bot without authentication...", "[SYSTEM]", Global.sysColor);
+                botUseCommand(() => bot.Start(false));
+            }
+            else if (args[0] == "stop" || args[0] == "idle")
+            {
+                botUseCommand(() => bot.changeState(State.IDLE));
+            }
+            else if (args[0] == "kill")
+            {
+
+                writef("Killing bot.", "[SYSTEM]", Global.sysColor);
+                botUseCommand(() => bot.Kill());
+            }
+            else if (args[0] == "help")
+            {
+                if (args.Length > 1)
+                    help(args[1]);
+                else
+                    DisplayHelp();
+
+            }
+            else if (args[0] == "search" || args[0] == "ladder")
+            {
+                botUseCommand(() => bot.changeState(State.SEARCH));
+            }
+            else if (args[0] == "version")
+            {
+                writef("ShowdownBot v" + Global.VERSION, "system", Global.sysColor);
+            }
+            else if (args[0] == "format" || args[0] == "f")
+            {
+                if (paramCheck(2, args, args[0]))
+                    botUseCommand(() => bot.changeFormat(args[1]));
+            }
+            else if (args[0] == "challenge" || args[0] == "cp")
+            {
+                if (args.Length == 2)
+                    botUseCommand(() => bot.challenge(args[1]));
+                else
+                    botUseCommand(() => bot.challenge(bot.getOwner()));
+            }
+            else if (args[0] == "m" || args[0] == "mode" || args[0] == "module")
+            {
+                if (paramCheck(2, args, "m"))
+                {
+                    if (args[1] == "random" || args[1] == "r")
+                    {
+                        botUseCommand(() => bot.changeMode(ShowdownBot.Bot.AiMode.RANDOM));
+                        
+                    }
+                    else if (args[1] == "biased" || args[1] == "b")
+                    {
+                        botUseCommand(() => bot.changeMode(ShowdownBot.Bot.AiMode.BIAS));
+                    }
+                    else if (args[1] == "analytic" || args[1] == "a")
+                    {
+                         botUseCommand(() => bot.changeMode(ShowdownBot.Bot.AiMode.ANALYTIC));
+                    }
+                }
+
+            }
+            else if (args[0] == "dump" || args[0] == "dumplog")
+            {
+                botUseCommand(() => bot.saveLog());
+            }
+            else if (args[0] == "exit")
+            {
+                writef("Shutting down.", "[SYSTEM]", Global.sysColor);
+                writef("Killing bot.", "[SYSTEM]", Global.sysColor);
+                botUseCommand(() => bot.Kill());
+                Environment.Exit(0);
+                this.Close();
+            }
+            else if (args[0] == "info")
+            {
+                if (!bot.getStatus())
+                {
+                    write("No bot running.");
+                }
+                else
+                {
+                    botUseCommand(() => bot.printInfo());
+                }
+            }
+            else if (args[0] == "visible" || args[0] == "v")
+            {
+                //change visibility
+            }
+            else if (args[0] == "clear" || args[0] == "cls")
+            {
+                Console.Clear();
+            }
+            else if (args[0] == "learn" || args[0] == "l")
+            {
+
+                if (!paramCheck(2, args, "learn"))
+                {
+
+                    return;
+                }
+                if (args[1] == "d" || args[1] == "download")
+                {
+                    if (paramCheck(3, args, "learn"))
+                    {
+                        botUseCommand(() => bot.learn(Int32.Parse(args[2])));
+                    }
+                }
+                else
+                {
+                    botUseCommand(() => bot.learn(0));
+                }
+
+            }
+            else if (args[0] == "simulate" || args[0] == "sim")
+            {
+                botUseCommand(() => bot.simulate(args[1], args[2]));
+            }
+            else
+            {
+                writef("Undefined command " + t, "[SYSTEM]", Global.sysColor);
+
+            }
+
+        }
+    }
+}
---
+++ ShowdownBot/Console.Help.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShowdownBot
+{
+    /// <summary>
+    /// Contains the in-console help documentation for commands.
+    /// </summary>
+    public partial class Consol
+    {
+        private void DisplayHelp()
+        {
+
+            writef("Available commands are: challenge, clear, dump, exit, info,\n kill, module, " +
+                    "start, startf, stop, version", "[SYSTEM]", Global.sysColor);
+
+        }
+
+        private void help(string cmnd)
+        {
+            string desc = cmnd;
+            string alias = "None";
+            string arguments = "None";
+            if (cmnd == "learn")
+            {
+                desc = "learn: Starts the bot's replay learning and downloading features";
+                alias = "l";
+                arguments = "\t [d][#] - Download # replays.\n" +
+                         "\t \t [b] - Build database from replays.";
+            }
+            else if (cmnd == "search" || cmnd == "ladder")
+            {
+                desc = "search: The bot will search the ladder for an opponent";
+                alias = "ladder";
+
+            }
+            else if (cmnd == "challenge" || cmnd == "cp")
+            {
+                desc = "challenge: Have the bot challenge a player";
+                alias = "cp";
+                arguments = "\t [... =owner] - Challenge specified player.\n";
+
+            }
+            else if (cmnd == "format" || cmnd == "f")
+            {
+                desc = "format: Change the bot module's selected format.";
+                alias = "f";
+                arguments = "\t [...] - Format name";
+            }
+            else if (cmnd == "exit")
+            {
+                desc = "exit: Kills the bot and quits the program.";
+            }
+            else if (cmnd == "module" || cmnd == "m")
+            {
+                desc = "module: Switch the bot's AI module.";
+                alias = "mode, m";
+                arguments = "\t [...] - Module (Analytic, Random, Biased)";
+            }
+            else if (cmnd == "me")
+            {
+                writef("I'm a robot, not a miracle worker.", "system", Global.sysColor);
+                return;
+            }
+
+            writef(desc + "\n" +
+                    "alias: " + alias + "\n" +
+                    "arguments: " + arguments, "system", Global.sysColor);
+        }
+    }
+}

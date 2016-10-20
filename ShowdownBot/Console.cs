09e9ecdee1378711309fe8cbe8e7b8bc94210742
Deviach <therixy@gmail.com>
2016-10-20 19:16:31 -0400

    varying color for updater

--- ShowdownBot/Console.cs
+++ ShowdownBot/Console.cs
@@ -155,22 +155,26 @@ namespace ShowdownBot
             string[] components = versiontext.Split('.');
             string[] mycomponents = SDB_VERSION.TrimEnd("-unreleased".ToCharArray()).Split('.');
             bool behind = false;
+            ConsoleColor color = GlobalConstants.COLOR_OK;
             if (Int32.Parse(components[0]) > Int32.Parse(mycomponents[0]))
             {
                 behind = true;
+                color = COLOR_ERR;
             }
             else if (Int32.Parse(components[1]) > Int32.Parse(mycomponents[1]))
             {
                 behind = true;
+                color = COLOR_WARN;
             }
             else if ((Int32.Parse(components[2]) > Int32.Parse(mycomponents[2]))
                 && (Int32.Parse(components[1]) == Int32.Parse(mycomponents[1])))
             {
                 behind = true;
+                color = COLOR_WARN;
             }
             if (behind)
             {
-                cwrite("There's a new version v" + versiontext + " available!","updater", COLOR_WARN);
+                cwrite("There's a new version v" + versiontext + " available!","updater", color);
             }
             else
                 cwrite("You have the latest version!","updater", COLOR_OK);
